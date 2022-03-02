e45f74bcf977ae0adad6a02cc7afabad3869eab8
RahmaRamadan <rahmaramadan23@gmailcom>
2022-03-02 17:58:29 +0200

    adding getPosts, getOnePost

--- GRABIRAPP/urls.py
+++ GRABIRAPP/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    
+    # ------------------------Post------------------------
     path('posts/', views.getPosts, name="get_posts"),
     path('posts/<str:post_id>', views.getOnePost, name="get_one_posts"),
+    path('add-post', views.addPost, name='add_post'),
+    path('edit-post/<str:post_id>', views.editPost, name='edit_post'),
+    path('delete-post/<str:post_id>', views.deletePost, name='delete_post'),
+
 ]
