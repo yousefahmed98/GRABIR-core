2b76972b4bc00adaaefce189fe7d4f3d9cdb0970
RahmaRamadan <rahmaramadan23@gmailcom>
2022-03-02 19:45:50 +0200

    adding users serializer

--- GRABIRAPP/serializers.py
+++ GRABIRAPP/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Post
+from .models import Post, CustomUser
 class PostSerializer (ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+
+class UserSerializer (ModelSerializer):
+    class Meta:
+        model = CustomUser
         fields = '__all__'
         
