819916c245a5873ac073f3845a944dd38c064678
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-03-02 19:33:57 +0200

    add-offers-api

--- GRABIRAPP/serializers.py
+++ GRABIRAPP/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Post
+from .models import Post, Offer
 class PostSerializer (ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+    
+class OfferSerializer (ModelSerializer):
+    class Meta:
+        model = Offer
         fields = '__all__'
         
