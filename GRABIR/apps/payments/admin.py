12b85650ecb7cc3376cc7bf5d23725a15724e2f4
RahmaRamadan <rahmaramadan23@gmailcom>
2022-03-04 21:16:09 +0200

    adding payments model

--- GRABIR/apps/payments/admin.py
+++ GRABIR/apps/payments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from GRABIR.apps.payments.models import Payment, PaymentStatus
+
 # Register your models here.
+admin.site.register(Payment)
+admin.site.register(PaymentStatus)
