db751de6f7ea13ee0568fc23486a5b82af11ebb7
RahmaRamadan <rahmaramadan23@gmailcom>
2022-03-08 23:54:10 +0200

    refactor offerStatus model

--- GRABIR/apps/offers/models.py
+++ GRABIR/apps/offers/models.py
@@ -3,15 +3,27 @@ from GRABIR.apps.base.models import CustomUser
 
 from GRABIR.apps.posts.models import Post
 
+
 class OfferStatus(models.Model):
-    status = models.CharField(max_length=50)  
+    LOCATOR_YES_NO_CHOICES = ((None, ''), (True, 'Yes'), (False, 'No'))
+    # status = models.BooleanField(nul)
+    status = models.BooleanField(choices=LOCATOR_YES_NO_CHOICES,
+                      max_length=3,
+                      blank=True, null=True, default=None,)
+
 
 class Offer(models.Model):
     details = models.CharField(max_length=300)
-    from_region= models.CharField(null=True,max_length=50)
-    to_region   = models.CharField(null=True,max_length=50) 
+    from_region = models.CharField(null=True, max_length=50)
+    to_region = models.CharField(null=True, max_length=50)
     price = models.FloatField()
-    delivery_date = models.DateField(auto_now_add=True,null=True)
-    post = models.ForeignKey(Post,related_name="offer_post", on_delete=models.CASCADE)
-    status = models.ForeignKey(OfferStatus,related_name="offer_status", on_delete=models.CASCADE)
-    offer_owner = models.ForeignKey(CustomUser, related_name="offer_user",on_delete=models.CASCADE)
+    delivery_date = models.DateField(auto_now_add=True, null=True)
+    post = models.ForeignKey(
+        Post, related_name="offer_post", on_delete=models.CASCADE)
+    status = models.ForeignKey(
+        OfferStatus, related_name="offer_status", on_delete=models.CASCADE, null=True)
+    offer_owner = models.ForeignKey(
+        CustomUser, related_name="offer_user", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.details
