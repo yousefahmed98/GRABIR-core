24cb38f558edd0776782a84dc39d56ad7e84e66e
youssefahmed98 <93130180+youssefahmed98@users.noreply.github.com>
2022-03-11 22:58:20 +0200

    login user done

--- GRABIR/apps/base/models.py
+++ GRABIR/apps/base/models.py
@@ -1,26 +1,40 @@
+from os import access
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
-
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your models here.
+
+
 class CustomUser(AbstractUser):
     region = models.CharField(max_length=200)
-    passport_img = models.FileField(upload_to='images/', null=True, verbose_name="passport image")
-    email = models.EmailField(_('email address'),blank=False,unique=True,db_index=True)
+    passport_img = models.FileField(
+        upload_to='images/', null=True, verbose_name="passport image")
+    email = models.EmailField(
+        _('email address'), blank=False, unique=True, db_index=True)
     is_verified = models.BooleanField(default=False)
-    def __str__ (self):
+
+    def __str__(self):
         return self.email
-    
+
     def tokens(self):
-        return ""
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
 
 
 class PhoneNumber(models.Model):
-    # phone =models.CharField(max_length=11) 
-    # phone = PhoneNumberField(unique = True, null = True, blank = False) 
-    phoneNumberRegex = RegexValidator(regex = r"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$")
-    phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 16, unique = True,null=True)
-    user = models.ForeignKey(CustomUser,related_name="phoneNumber_user", on_delete=models.CASCADE)
-    def __str__ (self):
+    # phone =models.CharField(max_length=11)
+    # phone = PhoneNumberField(unique = True, null = True, blank = False)
+    phoneNumberRegex = RegexValidator(
+        regex=r"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$")
+    phoneNumber = models.CharField(
+        validators=[phoneNumberRegex], max_length=16, unique=True, null=True)
+    user = models.ForeignKey(
+        CustomUser, related_name="phoneNumber_user", on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.phoneNumber
--- GRABIR/apps/base/serializers.py
+++ GRABIR/apps/base/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 from GRABIR.apps.base.models import PhoneNumber, CustomUser
+from rest_framework import serializers
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class UserSerializer (ModelSerializer):
@@ -7,8 +10,9 @@ class UserSerializer (ModelSerializer):
         model = CustomUser
         fields = '__all__'
         extra_kwargs = {
-            'password' : {'write_only':True}
+            'password': {'write_only': True}
         }
+
 
 class PhoneNumberSerializer (ModelSerializer):
     class Meta:
@@ -17,6 +21,40 @@ class PhoneNumberSerializer (ModelSerializer):
 
 
 class RegisterSerializer(ModelSerializer):
-     class Meta:
+    class Meta:
         model = CustomUser
         fields = ['email', 'username', 'password']
+
+
+class LoginSerializer(ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(
+        max_length=255, min_length=3, read_only=True)
+
+    tokens = serializers.SerializerMethodField()
+    def get_tokens(self, obj):
+        user = CustomUser.objects.get(email=obj['email'])
+
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+    class Meta:
+        model=CustomUser 
+        fields = ['email','password','username','tokens']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        user = auth.authenticate(username=email, password=password)
+        if not user:
+            raise AuthenticationFailed("'invalid email or password") 
+        if not user.is_verified:
+            raise AuthenticationFailed("Email not verified")
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
