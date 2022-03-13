600f191b5c617c3a2d23ab313b0c912439a7e1b6
youssefahmed98 <93130180+youssefahmed98@users.noreply.github.com>
2022-03-14 00:39:09 +0200

    remove wrong import

--- GRABIR/apps/offers/admin.py
+++ GRABIR/apps/offers/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from GRABIR.apps.offers.models import Offer, OfferStatus
+from GRABIR.apps.offers.models import Offer
 
 # Register your models here.
 admin.site.register(Offer)
-admin.site.register(OfferStatus)
