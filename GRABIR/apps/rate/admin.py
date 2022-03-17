a8335e61c4cd092aa33438f2743322faedadd57b
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-03-17 14:43:19 +0200

    rate admin

--- GRABIR/apps/rate/admin.py
+++ GRABIR/apps/rate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+
+from GRABIR.apps.rate.models import Rate
 # Register your models here.
+admin.site.register(Rate)
--- GRABIR/apps/rate/models.py
+++ GRABIR/apps/rate/models.py
@@ -6,7 +6,8 @@ from GRABIR.apps.base.models import CustomUser
 class Rate(models.Model):
     stars = models.IntegerField(max_length=5)
     review = models.CharField(max_length=30)
-    user = models.ForeignKey(CustomUser, related_name="rate_user", on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name="rate_user", on_delete=models.CASCADE, null=True)
+    reviewerName = models.ForeignKey(CustomUser, related_name="reviewer_user", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.stars
+        return self.review
